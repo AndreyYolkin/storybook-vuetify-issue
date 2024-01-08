1cd8fd031c7ea818bc506c6bc943b974f2f9d967
Andrey Yolkin <andreyyolkin@gmail.com>
2024-01-08 13:47:47 +0300

    fix: load vuetify plugins after vue plugins

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,5 +1,5 @@
-import type { StorybookConfig } from "@storybook/vue3";
-
+import type { StorybookConfig } from "@storybook-vue/nuxt";
+import {ViteConfig} from "@nuxt/schema";
 
 const config: StorybookConfig = {
   stories: [
@@ -18,5 +18,24 @@ const config: StorybookConfig = {
   docs: {
     autodocs: "tag",
   },
+  /** Vuetify plugins must be added after Vue plugin  */
+  viteFinal: (config: ViteConfig) => {
+    config.plugins = config.plugins || [];
+    const vuePluginIndex = config.plugins.findIndex((plugin) => {
+      return plugin.name === "vite:vue";
+    });
+    const vuetifyPlugins = config.plugins.filter((plugin) => {
+      return plugin.name && plugin.name.startsWith("vuetify:");
+    });
+    
+    config.plugins = [
+      ...config.plugins.slice(0, vuePluginIndex + 1).filter(
+        (plugin) => !plugin.name || !plugin.name.startsWith("vuetify:")
+      ),
+      ...vuetifyPlugins,
+      ...config.plugins.slice(vuePluginIndex + 1),
+    ];
+    return config
+  }
 };
 export default config;
